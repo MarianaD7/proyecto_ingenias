ab75e4f6a3f03060b0d3b0f61df803611291a13a
aureana86 <aureanita2286@gmail.com>
2023-09-12 19:53:09 -0300

    agregado del prompt en pag de contacto

--- script.js
+++ script.js
@@ -15,29 +15,3 @@ window.onload = function () {
         alert("No se enviará un mensaje de recordatorio.");
     }
 };
-
-// Evento para manejar el envío del formulario
-document.getElementById("formulario").addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Prevenir el envío del formulario para fines de demostración
-
-    // Aquí puedes agregar la lógica para enviar el formulario con el número de celular, si es necesario
-});
-
-/* let fechaCita = prompt("Por favor, ingrese la fecha y hora de su cita (Ejemplo: 10/09 a las 15:30):");
-if (fechaCita) {
-  alert(`Su cita está programada para el ${fechaCita}. ¡Gracias!`);
-} */
-/* let recordatorioCelular = confirm("¿Desea recibir un mensaje de recordatorio de su cita en su celular?");
-
-if (recordatorioCelular) {
-    let numeroCelular;
-
-    do {
-        numeroCelular = prompt("Por favor, ingrese su número de celular (solo números):");
-    } while (!/^\d+$/.test(numeroCelular));
-
-    alert(`Se enviará un mensaje de recordatorio a ${numeroCelular}. ¡Gracias!`);
-} else {
-    alert("No se enviará un mensaje de recordatorio.");
-} */
