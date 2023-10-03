97966e425759443cce7b704fa3354fe1b875fcb2
aureana86 <aureanita2286@gmail.com>
2023-10-03 19:17:38 -0300

    validacion datos de formularios

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function solicitarNumeroCelular() {
+/* function solicitarNumeroCelular() {
     let recordatorioCelular = confirm("¿Desea recibir un mensaje de recordatorio de su cita en su celular?");
 
     if (recordatorioCelular) {
@@ -13,54 +13,59 @@ function solicitarNumeroCelular() {
     } else {
         alert("No se enviará un mensaje de recordatorio.");
     }
-}
-solicitarNumeroCelular();
+} */
+/* solicitarNumeroCelular(); */
 
-function validarFormulario() { // Obtener valores de los campos
-    var nombre = document.getElementById("nombre").value.trim();
-    var apellido = document.getElementById("apellido").value.trim();
-    var telefono = document.getElementById("telefono").value.trim();
-    var email = document.getElementById("email").value.trim();
+// Obtener el formulario por su ID
+let formulario = document.getElementById("miFormulario");
+// Agregar un evento "submit" al formulario
+formulario.addEventListener("submit", function (e) { // Agregar un evento "submit" al formulario
+    e.preventDefault();
 
-    // Validar nombre (al menos 2 caracteres)
-    if (nombre.length < 2) {
-        alert(`El nombre debe tener al menos 2 caracteres.`);
-        return false;
+    // Obtener los valores de los campos
+    let nombre = document.getElementById("nombre").value.trim();
+    let apellido = document.getElementById("apellido").value.trim();
+    let telefono = document.getElementById("telefono").value.trim();
+    let email = document.getElementById("email").value.trim();
+
+    // Validar nombre (mas de 2 caracteres)
+    if (nombre.length <= 2) {
+        alert(`El nombre debe tener más de 2 caracteres.`);
+        return;
     }
+
     // Validar el apellido (Solo letras)
     if (/\d/.test(apellido)) {
         alert("El apellido no debe contener números.");
-        return false;
+        return;
     }
 
     // Validar teléfono (solo números)
     if (!/^\d+$/.test(telefono)) {
         alert("El número de contacto solo debe contener números.");
-        return false;
+        return;
     }
 
     // Validar correo electrónico (contiene "@" y al menos un carácter antes y después de "@")
     if (!/^.+@.+\..+$/.test(email)) {
         alert("Por favor, ingrese un correo electrónico válido.");
-        return false;
+        return;
     }
 
-    return true;
-}
-
+});
 function enviarFormulario() { // Obtener los valores de los campos
-    var nombre = document.querySelector('#nombre').value;
-    var apellido = document.querySelector('#apellido').value;
-    var telefono = document.querySelector('#telefono').value;
-    var email = document.querySelector('#email').value;
+    let nombre = document.querySelector('#nombre').value;
+    let apellido = document.querySelector('#apellido').value;
+    let telefono = document.querySelector('#telefono').value;
+    let email = document.querySelector('#email').value;
 
     // Obtener las elecciones de los checkboxes
-    var turnos = document.querySelector('input[name="turnos"]').checked;
-    var tratamiento = document.querySelector('input[name="tratamiento"]').checked;
-    var consulta = document.querySelector('input[name="consulta"]').checked;
+    let turnos = document.querySelector('input[name="turnos"]').checked;
+    let tratamiento = document.querySelector('input[name="tratamiento"]').checked;
+    let consulta = document.querySelector('input[name="consulta"]').checked;
 
     // Crear un objeto con los datos del formulario
-    var formData = {
+    let formData = {
         nombre: nombre,
         apellido: apellido,
         telefono: telefono,
@@ -71,7 +76,7 @@ function enviarFormulario() { // Obtener los valores de los campos
     };
 
     // Obtener la base de datos existente o crear una nueva
-    var baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos')) || [];
+    let baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos')) || [];
 
     // Agregar los datos del formulario a la base de datos
     baseDeDatos.push(formData);
@@ -86,40 +91,55 @@ function enviarFormulario() { // Obtener los valores de los campos
     return false;
 }
 // Función para mostrar la información almacenada
-function mostrarInformacionAlmacenada() {
-    // Obtener la base de datos desde el localStorage
-    var baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos')) || [];
+function mostrarInformacionAlmacenada() { // Obtener la base de datos desde el localStorage
+    let baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos')) || [];
 
     // Mostrar la información almacenada (puedes personalizar cómo se muestra)
-    var infoContainer = document.getElementById('infoContainer');
+    let infoContainer = document.getElementById('infoContainer');
     infoContainer.innerHTML = ''; // Limpiar el contenedor
 
     baseDeDatos.forEach(function (formData, index) {
-        var infoDiv = document.createElement('div');
+        let infoDiv = document.createElement('div');
         infoDiv.classList.add('infoEntry');
-        infoDiv.innerHTML = `<h3>Entrada ${index + 1}:</h3>
-        <p>Nombre: ${formData.nombre} ${formData.apellido}</p>
-        <p>Teléfono: ${formData.telefono}</p>
-        <p>Email: ${formData.email}</p>
-        <p>Elecciones: ${formData.eleccionTurnos}, ${formData.eleccionTratamiento}, ${formData.eleccionConsulta}</p>`;
+        infoDiv.innerHTML = `<h3>Entrada ${
+            index + 1
+        }:</h3>
+        <p>Nombre: ${
+            formData.nombre
+        } ${
+            formData.apellido
+        }</p>
+        <p>Teléfono: ${
+            formData.telefono
+        }</p>
+        <p>Email: ${
+            formData.email
+        }</p>
+        <p>Elecciones: ${
+            formData.eleccionTurnos
+        }, ${
+            formData.eleccionTratamiento
+        }, ${
+            formData.eleccionConsulta
+        }</p>`;
         infoContainer.appendChild(infoDiv);
     });
 }
 // Función para agregar un nuevo elemento a la base de datos
 function agregarNuevoElemento() {
-    var nombreNuevo = prompt("Ingrese un nuevo nombre:");
-    var apellidoNuevo = prompt("Ingrese un nuevo apellido:");
-    var telefonoNuevo = prompt("Ingrese un nuevo número de teléfono:");
-    var emailNuevo = prompt("Ingrese un nuevo correo electrónico:");
+    let nombreNuevo = prompt("Ingrese un nuevo nombre:");
+    let apellidoNuevo = prompt("Ingrese un nuevo apellido:");
+    let telefonoNuevo = prompt("Ingrese un nuevo número de teléfono:");
+    let emailNuevo = prompt("Ingrese un nuevo correo electrónico:");
 
-    var nuevoFormData = {
+    let nuevoFormData = {
         nombre: nombreNuevo,
         apellido: apellidoNuevo,
         telefono: telefonoNuevo,
         email: emailNuevo
     };
 
-    var baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos')) || [];
+    let baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos')) || [];
     baseDeDatos.push(nuevoFormData);
     localStorage.setItem('baseDeDatos', JSON.stringify(baseDeDatos));
     mostrarInformacionAlmacenada();
